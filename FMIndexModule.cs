fc7d32e17ba35289be4151da04d5f00534e98ac9
Anastasiia Byvsheva <gnomus042@gmail.com>
2021-06-02 01:14:19 -0700

    Initial

---
+++ FMIndexModule.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Threading;
+using Parcs;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+
+namespace FirstModule
+{
+    public class FMIndexModule : IModule
+    {
+
+        private static bool CompareSuffixAt(String text, int a, int b, int letter)
+        {
+            if (a + letter < text.Length && b + letter < text.Length)
+            {
+                return text[a + letter] < text[b + letter];
+            }
+            if (a + letter >= text.Length && b + letter < text.Length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool CompareSuffix(String text, int a, int b, int minLetter)
+        {
+            while (!CompareSuffixAt(text, a, b, minLetter) && !CompareSuffixAt(text, b, a, minLetter))
+            {
+                minLetter++;
+            }
+            return CompareSuffixAt(text, a, b, minLetter);
+        }
+
+        private static void Swap(List<Int32> order, int a, int b)
+        {
+            int tmp = order[a];
+            order[a] = order[b];
+            order[b] = tmp;
+        }
+
+        private static int SplitBy(String text, List<Int32> order, int left, int right, int by, int minLetter)
+        {
+            int i = left;
+            int index = -1;
+            for (int j = left; j < right; j++)
+            {
+                if (order[j] == by || CompareSuffix(text, order[j], by, minLetter))
+                {
+                    if (order[j] == by) index = i;
+                    Swap(order, i, j);
+                    i++;
+                }
+            }
+            Swap(order, i - 1, index);
+            return i - 1;
+        }
+
+        private static void BuildSuffixArray(
+            String text, List<Int32> order,
+            int left, int right,
+            char leftLetter = '$', char rightLetter = 'z', int minLetter = 0
+            )
+        {
+            if (left >= right - 1) return;
+            if (leftLetter == rightLetter)
+            {
+                //minLetter++;
+                leftLetter = '$';
+                rightLetter = 'z';
+            }
+
+            int by = order[(left + right) / 2];
+            int center = SplitBy(text, order, left, right, by, minLetter);
+            BuildSuffixArray(text, order, left, center, leftLetter, text[by + minLetter], minLetter);
+            BuildSuffixArray(text, order, center + 1, right, text[by + minLetter], rightLetter, minLetter);
+        }
+
+        public static String BuildFMIndex(String text, List<Int32> order)
+        {
+            BuildSuffixArray(text, order, 0, order.Count);
+            StringBuilder builder = new StringBuilder();
+            foreach (int s in order)
+            {
+                int pos = (s + text.Length - 1) % text.Length;
+                builder = builder.Append(text[pos]);
+            }
+            return builder.ToString();
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            String text = info.Parent.ReadString();
+            List<Int32> order = info.Parent.ReadObject<List<Int32>>();
+            info.Parent.WriteData(BuildFMIndex(text, order));
+        }
+    }
+}
---
+++ MainModule.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Parcs;
+
+namespace FirstModule
+{
+    class MainModule : IModule
+    {
+        static int tasks = 2;
+        static String text = "AABB";
+
+        static void Test()
+        {
+            String text = "daxfvbi$";
+            List<Int32> order = new List<Int32>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                order.Add(i);
+            }
+            String res = FMIndexModule.BuildFMIndex(text, order);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of tasks:");
+            tasks = Convert.ToInt32(Console.ReadLine());
+
+            text = File.ReadAllText("text.txt");
+
+            text = text.Replace("\n", "");
+            text = text.Replace("\r", "");
+            text = text.Replace("\0", "");
+            text = text.Replace("\t", "");
+            text = text.Replace(" ", "");
+
+            Console.WriteLine("Start!");
+
+            var job = new Job();
+            if (!job.AddFile(Assembly.GetExecutingAssembly().Location))
+            {
+                Console.WriteLine("File doesn't exist");
+                return;
+            }
+
+            (new MainModule()).Run(new ModuleInfo(job, null));
+            Console.ReadKey();
+        }
+
+        public void Run(ModuleInfo info, CancellationToken token = default(CancellationToken))
+        {
+            var points = new IPoint[tasks];
+            var channels = new IChannel[tasks];
+            for (int i = 0; i < tasks; ++i)
+            {
+                points[i] = info.CreatePoint();
+                channels[i] = points[i].CreateChannel();
+                points[i].ExecuteClass("FirstModule.FMIndexModule");
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            SortedDictionary<String, List<Int32>> parts = new SortedDictionary<String, List<int>>();
+            for (int i = 0; i < text.Length; i++)
+            {
+                string key = "" + text[i];
+                if (!parts.ContainsKey(key)) parts.Add(key, new List<Int32>());
+                parts[key].Add(i);
+            }
+
+            int taskSize = text.Length / tasks;
+
+
+            List<List<String>> a = new List<List<String>>();
+            a.Add(new List<String>());
+
+            var keys = parts.Keys;
+
+            int total = 0;
+            foreach (var entry in parts)
+            {
+                total += entry.Value.Count;
+                a[a.Count - 1].Add(entry.Key);
+                if (total > taskSize)
+                {
+                    total = 0;
+                    a.Add(new List<String>());
+                }
+            }
+
+            Parallel.For(0, tasks, (i) =>
+            {
+                {
+                    var order = new List<Int32>();
+                    foreach (String key in a[i])
+                    {
+                        order.AddRange(parts[key]);
+                    }
+                    channels[i].WriteData(text);
+                    channels[i].WriteObject(order);
+                    Console.WriteLine("Task #" + i.ToString() + " started. Length: " + order.Count.ToString() + ".");
+                }
+            });
+
+            String bwt = "";
+
+            for (int i = 0; i < tasks; ++i)
+            {
+                bwt += channels[i].ReadString();
+            }
+            stopWatch.Stop();
+            Console.WriteLine("Time: " + stopWatch.Elapsed);
+            if (bwt.Length > 16)
+            {
+                Console.WriteLine("BWT Transform: (too long, written to the file res.txt)");
+                Console.WriteLine(bwt.Substring(0, 16) + "...");
+                File.WriteAllLines("res.txt", new string[] { bwt });
+            }
+            else
+            {
+                Console.WriteLine("BWT Transform:");
+                Console.WriteLine(bwt);
+            }
+        }
+    }
+}
